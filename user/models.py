d07452e2d5fe85cb86fe86ea91b069aa7aa0d0e8
Jacob <jacob.juhyung.lee@gmail.com>
2020-12-29 23:16:48 +0900

    Add: models.py 완성 - reveiws 테이블에 ForiegnKey로 users 테이블 연결시켜주었습니다. - user_coupons라는 중간 테이블을 생성해 기존 쿠폰 테이블과 유저 아이디, 예약 테이블까지 연결 완료하였습니다.

---
+++ user/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+class SocialPlatform(models.Model):
+    name      = models.CharField(max_length=45)
+    username  = models.CharField(max_length=45)
+
+    class Meta:
+        db_table = 'social_platforms'
+
+class User(models.Model):
+    name                = models.CharField(max_length=45)
+    email               = models.EmailField(max_length=100, unique=True)
+    password            = models.CharField(max_length=250)
+    is_location_agreed  = models.BooleanField(default=False)
+    is_promotion_agreed = models.BooleanField(default=False)
+    social_platform     = models.ForeignKey(SocialPlatform, on_delete=models.CASCADE, default=null)
+    is_active           = models.BooleanField(default=False)
+    created_at          = models.DateTimeField(auto_now_add=True)
+    updated_at          = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'users'
+
+class RecentView(models.Model):
+    flight    = models.ForeignKey(flight.Flight, on_delete=models.CASCADE)
+    user      = models.ForeignKey(User, on_delete=models.CASCADE)
+    viewed_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'recent_views'
